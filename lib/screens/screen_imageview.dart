300603f6f3b4f145a57506bc32f953ee16448447
robin <83653446+perperam@users.noreply.github.com>
2022-12-06 22:50:46 +0100

    integration of image view

--- lib/screens/screen_imageview.dart
+++ lib/screens/screen_imageview.dart
@@ -1,25 +1,44 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:blindspot/image_view.dart' as iv;
 
-class ImageView extends StatelessWidget {
+class ImageView extends StatefulWidget  {
   const ImageView({super.key});
+
+  @override
+  State<ImageView> createState() => _ImageView();
+}
+
+
+class _ImageView extends State<ImageView> {
+
+  Future<Map<String, dynamic>> loadPic() async {
+    String data = await DefaultAssetBundle.of(context).loadString("assets/test_image_data.json");
+    return jsonDecode(data);
+  }
+
+  late Future<Map<String, dynamic>> futureMap;
+
+  @override
+  void initState() {
+    super.initState();
+    futureMap = loadPic();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('image')),
-        body: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                height: 200,
-                width: 150,
-                color: Colors.amber,
-              ),
-              Card(
-                  child: Center(
-                      child: Text('This is the images metadata\nData: 2022-10-24\nLocation: There'))),
-              ElevatedButton(
-                  onPressed: () => Navigator.pop(context), child: Text('Back'))
-            ]));
+        appBar: AppBar(title: const Text('Preview Page')),
+        body: FutureBuilder<Map<String, dynamic>>(
+            future: futureMap,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return iv.View(snapshot.data!);
+              } else {
+                return const iv.Waiting();
+              }
+            }
+        )
+    );
   }
 }
