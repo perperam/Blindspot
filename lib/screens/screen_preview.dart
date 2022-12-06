03099c59562c2aafb7ad4a30acefc9dc3021ba52
robin <83653446+perperam@users.noreply.github.com>
2022-12-06 01:10:53 +0100

    started a sorted metadata view

--- lib/screens/screen_preview.dart
+++ lib/screens/screen_preview.dart
@@ -1,10 +1,10 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+
 // import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 // import 'dart:io' as Io;
-
 
 class PreviewPage extends StatefulWidget {
   const PreviewPage({Key? key, required this.picture}) : super(key: key);
@@ -20,7 +20,8 @@ class _PreviewPageState extends State<PreviewPage> {
 
   Future<Map<String, dynamic>> postPic() async {
     var request = http.MultipartRequest('POST', apiUrlPicture);
-    request.files.add(await http.MultipartFile.fromPath('image', widget.picture.path)); // HERE IS THE ERROR
+    request.files.add(await http.MultipartFile.fromPath(
+        'image', widget.picture.path)); // HERE IS THE ERROR
 
     var streamedResponse = await request.send();
     final responseString = await http.Response.fromStream(streamedResponse);
@@ -39,36 +40,91 @@ class _PreviewPageState extends State<PreviewPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Preview Page')),
-      body: Center(
-        child: SingleChildScrollView(
-    child: Column(mainAxisSize: MainAxisSize.min, children: [
+        appBar: AppBar(title: const Text('Preview Page')),
+        body: SingleChildScrollView(
+            child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
           FutureBuilder<Map<String, dynamic>>(
-            future: futureMap,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Image.memory(base64Decode(snapshot.data!["image"]));
-              } else {
-                return const CircularProgressIndicator();
-              }
-            }
-          ),
+              future: futureMap,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Image.memory(base64Decode(snapshot.data!["image"]));
+                } else {
+                  return const CircularProgressIndicator();
+                }
+              }),
           // Image.file(File(widget.picture.path), fit: BoxFit.cover, width: 250),
           const SizedBox(height: 24),
           Text(widget.picture.name),
           FutureBuilder<Map<String, dynamic>>(
-            future: futureMap,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data!["metadata"]);
-              } else {
-                return const CircularProgressIndicator();
-              }
-            }
-          )
-        ])
-        )
-      ),
+              future: futureMap,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return MetaView(snapshot.data!["metadata"]);
+                  // return Text(snapshot.data!["metadata"]);
+                } else {
+                  return const CircularProgressIndicator();
+                }
+              })
+        ])));
+  }
+}
+
+// class ImageClasse {
+//   final String name;
+//   final int classnumber;
+//   final double confidence;
+//   final double xmin;
+//   final double ymin;
+//   final double xmax;
+//   final double ymax;
+//
+//   const ImageClasse({
+//     required  this.name,
+//     required  this.classnumber,
+//     required  this.confidence,
+//     required  this.xmin,
+//     required  this.ymin,
+//     required  this.xmax,
+//     required  this.ymax
+// })
+// }
+
+class MetaView extends StatelessWidget {
+  final String metadata;
+
+  const MetaView(this.metadata);
+
+  @override
+  Widget build(BuildContext context) {
+    print(metadata.runtimeType);
+    List data = json.decode(metadata);
+    for (var d in data) {
+      print("OBJEKT: $d");
+    }
+
+    List<Widget> textChildren = [];
+    for (var d in data) {
+      // print(d.runtimeType);
+      textChildren.add(Container(
+        color: Colors.orangeAccent,
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("name: ${d["name"].toString()}"),
+          Text("name: ${d["class"].toString()}"),
+          Text("confidence: ${d["confidence"].toString()}")
+        ],
+      )));
+      // textChildren.add(
+      //     Container(color: Colors.orangeAccent, child: Text(d.toString())));
+      textChildren.add(Container(color: Colors.white, height: 20));
+    }
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: textChildren,
     );
   }
 }
