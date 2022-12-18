cb5b6fea36d0c0c2b00d29e334f16102d677a35e
robin <83653446+perperam@users.noreply.github.com>
2022-12-18 10:55:48 +0100

    new login logic

--- lib/reusable/login_state.dart
+++ lib/reusable/login_state.dart
@@ -4,8 +4,6 @@ import '../screens/screen_home.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginState extends StatefulWidget {
-  //final bool value;
-  //const MyLogin({Key? key, required this.value}) : super(key: key);
   LoginState({Key? key}) : super(key: key);
 
   @override
@@ -13,35 +11,13 @@ class LoginState extends StatefulWidget {
 }
 
 class _LoginState extends State<LoginState> {
-  User? user; //track the authenticated user here
-  // final emailInput = TextEditingController(text: '');
-  // final passInput = TextEditingController(text: '');
-  // FirebaseAuth auth = FirebaseAuth.instance;
   bool valueUser = false;
 
   @override
   Widget build(BuildContext context) {
-    // if (FirebaseAuth.instance.currentUser() != null) {
-    //   return HomeScreen();
-    // } else {
-    //   return LoginScreen();
-    // }
-    FirebaseAuth.instance.userChanges().listen((User? user) {
-      if (user == null) {
-        valueUser = false;
-      } else {
-        valueUser = true;
-      }
-      print('VALUE USER INSIDE: $valueUser');
-    });
-    print('VALUE USER OUTSIDE: $valueUser');
-
-    if (valueUser != true) {
-      print("DO: GO TO HOME SCREEN");
+    if (FirebaseAuth.instance.currentUser != null) {
       return HomeScreen();
     } else {
-      valueUser = false;
-      print("DO: GO TO HOME SCREEN");
       return LoginScreen();
     }
   }
